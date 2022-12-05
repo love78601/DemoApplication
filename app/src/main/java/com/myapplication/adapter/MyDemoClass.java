921b129714781ee3c1941001b2b55ceeb7041f77
Love <love.03verma@gmail.com>
2022-12-05 15:13:29 +0530

    merge changes

--- app/src/main/java/com/myapplication/adapter/MyDemoClass.java
+++ app/src/main/java/com/myapplication/adapter/MyDemoClass.java
@@ -1,9 +1,5 @@
 package com.myapplication.adapter;
 
 public class MyDemoClass {
-<<<<<<< HEAD
     String name="love_verma_h";
-=======
-    String name="love_verma_";
->>>>>>> a911d82d4cb538a1b448102d44764a8c2e6103cb
 }
