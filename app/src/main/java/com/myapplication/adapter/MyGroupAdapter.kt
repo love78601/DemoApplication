8cae97120a5e0054209565992a7fb36b34569de6
Love <love.03verma@gmail.com>
2022-12-06 13:21:49 +0530

    Name field added in app

--- app/src/main/java/com/myapplication/adapter/MyGroupAdapter.kt
+++ app/src/main/java/com/myapplication/adapter/MyGroupAdapter.kt
@@ -6,7 +6,7 @@ import com.xwray.groupie.ViewHolder
 
 
 class MyGroupAdapter : GroupAdapter<ViewHolder>() {
-
+   var name:String="lovevvvv"
     companion object {
         var currentPosition: Int = -1
         private var recyclerViewListener: RecyclerViewListener? = null
